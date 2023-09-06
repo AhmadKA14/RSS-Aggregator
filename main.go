c2a63be0f690f73fc410dc108cbb2ab51ad75ab1
ahmad <ahmadkhalil822003@gmail.com>
2023-09-06 15:38:45 -0400

    Added feed scraping functionality

--- main.go
+++ main.go
@@ -2,9 +2,11 @@ package main
 
 import (
 	"database/sql"
+	"fmt"
 	"log"
 	"net/http"
 	"os"
+	"time"
 
 	"github.com/ahmadka14/rssagg/internal/database"
 	"github.com/go-chi/chi"
@@ -19,6 +21,12 @@ type apiConfig struct {
 }
 
 func main() {
+	feed, err := urlToFeed("https://wagslane.dev/index.xml")
+	if err != nil {
+		log.Fatal(err)
+	}
+	fmt.Println(feed)
+
 	godotenv.Load()
 
 	portString := os.Getenv("PORT")
@@ -36,9 +44,12 @@ func main() {
 		log.Fatal("Can't connect to database")
 	}
 
+	db := database.New(conn)
 	apiCfg := apiConfig{
-		DB: database.New(conn),
+		DB: db,
 	}
+
+	go startScraping(db, 10, time.Minute)
 
 	router := chi.NewRouter()
 
